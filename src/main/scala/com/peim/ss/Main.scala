7512048267cc4810612c567e555c926c2f0ff8b0
Ivan Petrov <ivanpetrov0005@gmail.com>
2017-01-22 09:58:19 +0300

    ConnectionFlow using

--- src/main/scala/com/peim/ss/Main.scala
+++ src/main/scala/com/peim/ss/Main.scala
@@ -7,12 +7,12 @@ import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.util.ByteString
 import com.typesafe.config.ConfigFactory
+
 import scala.concurrent.duration._
-import scala.xml._
-
-
+import scala.xml.XML
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
@@ -40,11 +40,15 @@ object Main extends App with RequestTimeout {
       system.terminate()
   }
 
-  val responseFuture =
-    Http.get(system).singleRequest(HttpRequest.apply(
-      HttpMethods.GET,
-      "http://yandex.ru/blogs/rss/search?text=scala&numdoc=10"
-    ));
+
+  val uri = "/blogs/rss/search?text=scala&numdoc=10"
+
+  val connectionFlow: Flow[HttpRequest, HttpResponse, Future[Http.OutgoingConnection]] =
+    Http().outgoingConnection("yandex.ru")
+  val responseFuture: Future[HttpResponse] =
+    Source.single(HttpRequest(uri = uri))
+      .via(connectionFlow)
+      .runWith(Sink.head)
 
   responseFuture.onComplete {
     case Success(result) => {
@@ -53,7 +57,7 @@ object Main extends App with RequestTimeout {
         .runReduce(_ + _)
         .onComplete {
         case Success(body) => {
-          val xml = scala.xml.XML.loadString(body)
+          val xml = XML.loadString(body)
           val links = (xml \\ "item" \\ "link").map(_.text).toVector
 
           print(links)
